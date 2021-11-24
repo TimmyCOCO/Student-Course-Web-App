7a2f88ab885b202dc2dc16d6dcc7dac060872aab
jayden <jayden087t@gmail.com>
2021-11-24 13:58:18 -0500

    big change

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -1,142 +1,77 @@
+const Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('d7umftqv5d5rmt', 'shawkzmkitxejw',
+    '04707ab0dbd2a011de81d271d15474fd81d4f7274c3cb4302fa3f18e92a5778a', {
+        host: 'ec2-52-205-6-133.compute-1.amazonaws.com',
+        dialect: 'postgres',
+        port: 5432,
+        dialectOptionns: {
+            ssl: { rejectUnauthorized: false }
+        },
+        query: { raw: true }
+    });
+
 
 // Initialize function
 module.exports.initialize = function () {
     return new Promise(function (resolve, reject) {
         reject();
     });
-        });
-    });
 }
 
 // provide the full array of "student" objects
 module.exports.getAllStudents = function () {
-    return new Promise((resolve, reject) => {
-        if (students.length > 0) {
-            resolve(students);
-        }
-
-        if (students.length == 0) {
-            reject("no results returned");
-            return;
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 }
 
 // provide an array of "student" object
 module.exports.getTAs = function () {
-    return new Promise((resolve, reject) => {
-        let TAsList = [];
-
-        if (students.length > 0) {
-            for (let i = 0; i < students.length; i++) {
-                if (students[i].TA == true) {
-                    TAsList.push(students[i])
-                }
-            }
-        }
-
-        if (students.length == 0) {
-            reject("no results returned");
-            return;
-        }
-
-        resolve(TAsList);
-    }
-    )
+    return new Promise(function (resolve, reject) {
+        reject();
+    });
 }
-
 
 // provide the full array of "course" objects
 module.exports.getCourses = function () {
-    return new Promise((resolve, reject) => {
-        if (courses.length > 0) {
-            resolve(courses);
-        }
-
-        if (courses.length == 0) {
-            reject("no results returned");
-            return;
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 }
 
 // provide the "course" objects matches course id
 module.exports.getCourseById = function (id) {
-    return new Promise((resolve, reject) => {
-        for (let i = 0; i < courses.length; i++) {
-            if (courses[i].courseId == id) {
-                resolve(courses[i]);
-            }
-        }
-        
-        if (!courses[i]) {
-            reject("query returned 0 results");
-            return;
-        }
-    })
+    return new Promise(function (resolve, reject) {
+        reject();
+    });
 }
 
 // provide an array of "student" objects matches course id
 module.exports.getStudentsByCourse = function (course) {
-    return new Promise((resolve, reject) => {
-        let studentsList = [];
-        if (students.length > 0) {
-            for (let i = 0; i < students.length; i++) {
-                if (students[i].course == course) {
-                    studentsList.push(students[i]);
-                }
-            }
-        }
-
-        if (studentsList.length == 0) {
-            reject("no results returned");
-            return;
-        }
-
-        resolve(studentsList);
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 }
 
 // provide a single "student" object matches student number
 module.exports.getStudentByNum = function (num) {
-    return new Promise((resolve, reject) => {
-        if (students.length > 0) {
-            resolve(students[num - 1]);
-        }
-
-        if (students.length == 0) {
-            reject("no result returned");
-            return;
-        }
-    })
+    return new Promise(function (resolve, reject) {
+        reject();
+    });
 }
 
 
 // add student 
 module.exports.addStudent = function (studentData) {
-    return new Promise((resolve, reject) => {
-        if (studentData) {
-            studentData.studentNum = students.length + 1;
-            studentData.TA = (studentData.TA) ? true : false;
-            students.push(studentData);
-            resolve();
-        } else {
-            reject();
-            return;
-        }
-    })
+    return new Promise(function (resolve, reject) {
+        reject();
+    });
 }
 
 // update student
 module.exports.updateStudent = function (studentData) {
-    return new Promise((resolve, reject) => {
-        for (let i = 0; i < students.length; i++) {
-            if (studentData.studentNum == students[i].studentNum) {
-                students[i] = studentData;
-                students[i].TA = (studentData.TA) ? true : false;
-            }
-        }
-        resolve();
-
-    })
+    return new Promise(function (resolve, reject) {
+        reject();
+    });
 }
